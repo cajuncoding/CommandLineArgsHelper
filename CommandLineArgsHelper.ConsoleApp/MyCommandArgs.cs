594139a6bc2cd4b674df4a1219c27b6306c9c53b
Brandon Bernard <brandon.bernard@exlrt.com>
2020-07-24 16:03:17 -0500

    Add example support for Strongly Typed class and fix AssertArgumentNotNull() extension to be chainable for better streamlined use in argument validation.

---
+++ CommandLineArgsHelper.ConsoleApp/MyCommandArgs.cs
@@ -0,0 +1,30 @@
+﻿using CommandLineArgsHelpers;
+using System;
+
+namespace CommandLineArgsHelperConsoleApp
+{
+    /// <summary>
+    /// Sample of a mapping into a Strongly Typed Command Arg class for pre-defined args that you would like to validate this way.
+    /// </summary>
+    public class MyCommandArgs
+    {
+        private CommandLineArgsHelper args = null;
+
+        public MyCommandArgs(CommandLineArgsHelper args)
+        {
+            this.args = args ?? throw new ArgumentNullException(nameof(args), "Command Line Parameters collection must be specified.");
+        }
+
+        public string Size => args["size"] ?? throw new ArgumentNullException("size", "The Size argument is Required!"); //Manual Required field via Null-Coalesce
+        public string Height => args["height"] ?? String.Empty; //Optional; default is empty string.
+        public string Day => args["day"]; //Optional; default is null
+        public string Tgif => args["tgif"].AssertArgumentNotNull("tgif"); //Required Parameter using Custom Extension Method.
+        public string AmINice => args["AmINice"];
+        public bool Debug => String.Equals(args["debug"], "true", StringComparison.OrdinalIgnoreCase); //Boolean value that must be 'true' to be True; case-insensitive.
+        public string Param1 => args["param1"];
+        public string Param2Alone => args["param2Alone"];
+        public string Param3 => args["param3"];
+        public string Param4 => args["param4"];
+        public string Param6_with_spaces => args["param 6 with spaces"];
+    }
+}
--- CommandLineArgsHelper.ConsoleApp/Program.cs
+++ CommandLineArgsHelper.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLineArgsHelpers;
+using Newtonsoft.Json;
 using System;
 
 namespace CommandLineArgsHelperConsoleApp
@@ -13,12 +14,31 @@ namespace CommandLineArgsHelperConsoleApp
 
             if (commandLineArgHelper.Parameters.Count > 0)
             {
-                Console.WriteLine($"Command Line Arguments:");
+                //First load and output the command args Dynamically (Loosely Coupled)...
+                Console.WriteLine("Dynamically processing the Command Line Args...");
+                Console.WriteLine($"Command Line Arguments [{commandLineArgHelper.Parameters.Count} found]:");
                 var x = 0;
                 foreach (var kv in commandLineArgHelper.Parameters)
                 {
                     Console.WriteLine($"   {++x}) [{kv.Key}] == [{kv.Value}]");
                 }
+
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to see Strongly Typed Command Class sample (rendered as Serialized Json)...");
+                Console.ReadLine();
+
+                //Now Load and render out the Strongly Typed Command Arg (Static/Tightly Coupled)...
+                Console.WriteLine();
+                Console.WriteLine($"Statically processing the Command Line Args (Serialized as Json)...");
+
+                var myCommandArgs = new MyCommandArgs(commandLineArgHelper);
+                var myCommandArgsJson = JsonConvert.SerializeObject(myCommandArgs, Formatting.Indented);
+
+                Console.WriteLine($"{nameof(MyCommandArgs)}:");
+                Console.WriteLine(myCommandArgsJson);
+
+                Console.WriteLine();
+                Console.WriteLine("Finished.");
             }
             else
             {
@@ -26,6 +46,7 @@ namespace CommandLineArgsHelperConsoleApp
             }
 
             Console.ReadLine();
+
         }
     }
 }
